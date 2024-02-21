5f244935683f61333502e3d5d1fb8ccccb971f5f
diarra192 <dboureima192@gmail.com>
2024-02-21 06:04:08 -0800

    Debutant

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-int main() {
-char letter;
-for (letter = 'a'; letter <= 'z'; letter++) {
-putchar(letter);
-}
-putchar('\n');
-return 0;
+int main(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	putchar('\n');
+	return (0);
 }
