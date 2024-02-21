b804e8f0057d6a2f64273a2066e39e779d64566d
diarra192 <dboureima192@gmail.com>
2024-02-21 06:55:30 -0800

    alphabet

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -5,14 +5,14 @@
  */
 int main(void)
 {
-    char c = 'a';
-
-    while (c <= 'z')
-        putchar(c++);
-    c = 'A';
-    while (c <= 'Z')
-        putchar(c++);
-    putchar('\n');
-    return (0);
+	char c = 'a';
+	
+	while (c <= 'z')
+		putchar(c++);
+        c = 'A';
+        while (c <= 'Z')
+		putchar(c++);
+        putchar('\n');
+        return (0);
 }
 
