07e3a28c560e7ee0567de0300e797b4964331d16
root <dboureima192@gmail.com>
2024-02-22 09:57:36 +0000

    4-print_alphabt.c

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -7,7 +7,8 @@ int main(void)
 {
 char letter;
 for (letter = 'a'; letter <= 'z'; letter++)
-if (letter != 'q' && letter != 'e') 
+{
+if (letter != 'e' && letter != 'q') 
 putchar(letter);
 }    
 putchar('\n'); 
