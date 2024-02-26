fdae19822adbe9d3caa18c8d640345a8b6371e67
diarra192 <dbouureima192@gmail.com>
2024-02-26 11:11:03 +0000

    3-print_numbers

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  * print_numbers
+ * Return: void
  */
 void print_numbers(void)
 {
-int i;
-for (i = 0; i < 10; i++)
-{
-_putchar(i + '0');
-}
-_putchar('\n');
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		_putchar(i);
+	_putchar('\n');
 }
