c6804434cbc25574d01fe39b36dc32f35bf46155
diarra192 <dbouureima192@gmail.com>
2024-02-26 10:26:17 +0000

    3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main"
+/**
+ * print_numbers - Imprime les nombres de 0 à 9, suivis d'une nouvelle ligne.
+ */
+void print_numbers(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+_putchar(i + '0');
+}
+_putchar('\n');
+}
