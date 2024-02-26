4400b7c95ddb8caf23c7ae4b915f632304ef341b
diarra192 <dbouureima192@gmail.com>
2024-02-26 14:40:03 +0000

    4-print_most_numbers

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,10 @@
 #include "main.h"
+
 /**
-* main - Prints all possible combinations
-* Return: Always 0.
-*/
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 void print_most_numbers(void)
 {
 int i;
