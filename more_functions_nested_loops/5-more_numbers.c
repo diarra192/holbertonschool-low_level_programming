865be409bbf35e40244f8d8e210fe6a455046b48
diarra192 <dboureima192@gmail.com>
2024-02-27 13:53:46 +0000

    5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -5,17 +5,22 @@
  * Return: Always 0.
  */
 void _putchar(char c) {
-    write(1, &c, 1);
+    putchar(c);
 }
 
 void more_numbers(void) {
     int i, j;
-    for (i = 0; i < 10; i++) { // Boucle 10 fois pour chaque ligne
-        for (j = 0; j <= 14; j++) { // Itérer de 0 à 14
-            if (j > 9) // Si le nombre a deux chiffres
-                _putchar('1'); // Imprimer le premier chiffre
-            _putchar((j % 10) + '0'); // Imprimer le dernier chiffre
-        }
-        _putchar('\n'); // Passer à la ligne suivante
+    for (i = 0; i <= 14; i++) {
+        j = i * 10;
+        _putchar((j / 10) + '0');
+        _putchar((j % 10) + '0');
+        _putchar(' ');
     }
+    _putchar('\n');
 }
+
+int main() {
+    more_numbers();
+    return 0;
+}
+
