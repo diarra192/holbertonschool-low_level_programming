9f13fce9d9e7c59e2930c5d42044ecc9ba73afd0
diarra192 <dboureima192@gmail.com>
2024-03-10 20:22:25 +0100

    0-puts_recursion.c

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,13 +1,19 @@
 #include "main.h"
+
 /**
- * _print_rev_recursion - Prints a string in reverse.
- * @s: The string to be printed.
+ * _puts_recursion -> Prints a string, followed by a new line.
+ *
+ * @s: Input
  */
-void _print_rev_recursion(char *s)
+
+void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\0')
+		_putchar('\n');
+	if (*s != '\0')
 	{
-		_print_rev_recursion(s + 1);
 		_putchar(*s);
+		s++;
+		_puts_recursion(s);
 	}
 }
