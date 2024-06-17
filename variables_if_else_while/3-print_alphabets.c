a05bfb607f41d0ed6e93db65ef5b25513f46ac39
diarra192 <dboureima192@gmail.com>
2024-06-17 15:13:25 +0200

    variable if

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main
+ * Return: 0
+ */
+int main(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+putchar(c++);
+}
+c = 'A';
+while (c <= 'Z')
+{
+putchar(c++);
+}
+putchar('\n');
+return (0);
+}
