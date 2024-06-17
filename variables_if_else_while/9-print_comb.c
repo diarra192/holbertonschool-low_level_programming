18aa93059527279705cddd7cb604405654a1973a
diarra192 <dboureima192@gmail.com>
2024-06-17 15:43:37 +0200

    variable

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,24 +1,33 @@
 #include <stdio.h>
+
 /**
- * main - main block
- * Write a program that prints all single digit
- * Return: 0
+ * main - bloc principal
+ * Écrit un programme qui affiche tous les chiffres uniques
+ * Retourne: 0
  */
 int main(void)
 {
-int num;
+    int num;
 
-for (num = 0; num <= 9; num++)
-{
-putchar((num % 10) + '0');
-if (num == 9)
-continue;
-		
-putchar(',');
-putchar(' ');
+    // Boucle pour parcourir les chiffres de 0 à 9
+    for (num = 0; num <= 9; num++)
+    {
+        // Affiche le chiffre actuel
+        putchar((num % 10) + '0');
+        
+        // Si le chiffre est 9, continuer à la prochaine itération (éviter la virgule et l'espace après le 9)
+        if (num == 9)
+            continue;
+        
+        // Affiche une virgule suivie d'un espace
+        putchar(',');
+        putchar(' ');
+    }
+
+    // Affiche une nouvelle ligne après avoir affiché tous les chiffres
+    putchar('\n');
+
+    // Retourne 0 pour indiquer que le programme s'est terminé avec succès
+    return (0);
 }
 
-putchar('\n');
-
-return (0);
-}
