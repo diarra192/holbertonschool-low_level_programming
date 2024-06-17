ea1327a11fa6d29a33815d1a281eee88008d1d98
diarra192 <dboureima192@gmail.com>
2024-06-17 15:55:45 +0200

    variable

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -7,27 +7,19 @@
  */
 int main(void)
 {
-    int num;
+int num;
 
-    // Boucle pour parcourir les chiffres de 0 à 9
-    for (num = 0; num <= 9; num++)
-    {
-        // Affiche le chiffre actuel
-        putchar((num % 10) + '0');
-        
-        // Si le chiffre est 9, continuer à la prochaine itération (éviter la virgule et l'espace après le 9)
-        if (num == 9)
-            continue;
-        
-        // Affiche une virgule suivie d'un espace
-        putchar(',');
-        putchar(' ');
-    }
-
-    // Affiche une nouvelle ligne après avoir affiché tous les chiffres
-    putchar('\n');
-
-    // Retourne 0 pour indiquer que le programme s'est terminé avec succès
-    return (0);
+for (num = 0; num <= 9; num++)
+{
+putchar((num % 10) + '0');
+if (num == 9)
+continue;
+		
+putchar(',');
+putchar(' ');
 }
 
+putchar('\n');
+
+return (0);
+}
