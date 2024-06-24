521f9a034e643c13cf8d65d4cbd7ca5c691ab2c5
diarra192 <dboureima192@gmail.com>
2024-06-24 18:12:20 +0200

    functions

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,33 +1,30 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * print_sign - check number
+ * @n : character to check
+ * Return: If pos = 1, if neg = -1, else =0
  */
-int main(void)
-{
-    int r;
 
-    r = print_sign(98);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(0);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(0xff);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(-1);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
+int print_sign(int n)
+{
+	int temp;
+
+		if (n > 0)
+		{
+			_putchar('+');
+			temp = 1;
+		}
+		if (n == 0)
+		{
+			_putchar('0');
+			temp = 0;
+		}
+		if (n < 0)
+		{
+			_putchar('-');
+			temp = -1;
+		}
+		return (temp);
 }
